7a096f9d558c4f803a57c23d9978514d6e9785fb
Darren Seet <darren_seet@oredi.com>
2018-05-27 20:22:54 -0400

    refactor: merge css into single file for each html Added new gulp packages to autoprefix css, minify and merge. Modify references in html file to point to minify version. Updated gulp to perform the css modification operations. Remove hardcoded browser prefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const del = require('del');
 const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const clean_css = require('gulp-clean-css');
+const concat = require('gulp-concat');
 const image_resize = require('gulp-image-resize');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
@@ -18,10 +21,25 @@ const build_html = () => {
         .pipe(gulp.dest(`${dirs.dest}/public/`));
 }
 
+const build_css_source = (source) => {
+    return gulp.src([
+            `${dirs.src}/public/css/styles.css`,
+            `${dirs.src}/public/css/${source}.css`
+        ])
+        .pipe(clean_css({ sourceMap: true }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat(`${source}.min.css`))
+        .pipe(gulp.dest(`${dirs.dest}/public/css`));
+}
+
+const build_css_index = () => build_css_source('index');
+const build_css_restaurant = () => build_css_source('restaurant');
+
+const build_css = gulp.series(build_css_index, build_css_restaurant);
+
 const copy_static = () => {
     return gulp.src([
         `${dirs.src}/public/js/*`,
-        `${dirs.src}/public/css/*`,
         `${dirs.src}/public/data/*`,
         `${dirs.src}/public/img/*`,
         `${dirs.src}/*.js`
@@ -29,7 +47,7 @@ const copy_static = () => {
     .pipe(gulp.dest(`${dirs.dest}`));
 };
 
-const build_all = gulp.series(clean, build_html, copy_static);
+const build_all = gulp.series(clean, build_html, build_css, copy_static);
 gulp.task('watch', () => {
     gulp.watch([dirs.src], build_all)
 });
