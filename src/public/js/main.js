33a1699cb65eaf1abf27337bc46b049090cd8c79
Darren Seet <darren_seet@oredi.com>
2018-07-13 20:56:38 -0400

    bug: Remove unused code
    Remove plugins and code to pipe GOOLGE_API_KEY

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -92,23 +92,6 @@ initMap = () => {
 
   updateRestaurants();
 }
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-
-  google.maps.event.addListenerOnce(self.map, 'idle', () => {
-    document.getElementsByTagName('iframe')[0].title = "Google Maps";
-  });
-
-  updateRestaurants();
-} */
 
 /**
  * Update page and map for current restaurants.
@@ -213,14 +196,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
       window.location.href = marker.options.url;
     }
   });
-} 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
+}
