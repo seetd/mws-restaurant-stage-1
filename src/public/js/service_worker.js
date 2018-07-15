a6ac96fc2d448407031c17f09cc0f2b9c435e26e
Darren Seet <darren_seet@oredi.com>
2018-07-15 07:58:06 -0400

    feat: Handle missing restaurant images
    If images are missing when loading in ServiceWorker, this handles the lazy loading issue

--- src/public/js/service_worker.js
+++ src/public/js/service_worker.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'rr-static-v2';
+const staticCacheName = 'rr-static-v3';
 const dynamicCacheName = 'rr-dynamic';
 
 const staticUrls = [
@@ -9,6 +9,7 @@ const staticUrls = [
     'css/index.min.css',
     'css/restaurant.min.css',
     'img/logo.svg',
+    'img/noimage.png',
     'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
     'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
 ];
@@ -17,15 +18,26 @@ const allCaches = [
     staticCacheName, dynamicCacheName
 ];
 
-const loadCacheOrNetwork = request => 
+const loadCacheOrNetwork = (request, requestUrl, isSameOrigin) => 
     caches.match(request)
         .then(response => response || fetch(request).then(
             networkResponse => {
-                const responseClone = networkResponse.clone();
-                caches.open(dynamicCacheName).then(function (cache) {
-                    cache.put(request, responseClone);
-                });
-                return networkResponse;
+                // Handle restaurant images missing
+                if (
+                    networkResponse.status === 404
+                    && isSameOrigin
+                    && requestUrl.pathname.startsWith('/img')
+                    && requestUrl.pathname.endsWith('.jpg')
+                ) {
+                    return caches.match('img/noimage.png')
+                        .then(noimageResponse => noimageResponse);
+                } else {
+                    const responseClone = networkResponse.clone();
+                    caches.open(dynamicCacheName).then(function (cache) {
+                        cache.put(request, responseClone);
+                    });
+                    return networkResponse;
+                }
             }
         ));
 
@@ -56,11 +68,15 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
     const requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
+        // Redirect 'http://localhost:8000' to 'http://localhost:8000/index.html' since 
+        // they should bascially be the same html
         if (requestUrl.pathname === '/') {
             event.respondWith(caches.match('index.html'));
             return;
         }
 
+        // Strip all query parameters so that 'http://localhost:8000/restaurant.html?id=1' 
+        // and 'http://localhost:8000/restaurant.html?id=2' are the same
         if (requestUrl.pathname.startsWith('/restaurant.html')) {
             event.respondWith(caches.match('restaurant.html'));
             return;
@@ -72,7 +88,7 @@ self.addEventListener('fetch', function (event) {
         return;
     }
     
-    event.respondWith(loadCacheOrNetwork(event.request));
+    event.respondWith(loadCacheOrNetwork(event.request, requestUrl, requestUrl.origin === location.origin));
 });
 
 self.addEventListener('message', function (event) {
